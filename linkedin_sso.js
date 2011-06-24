2075de65b053d10145641a194ed7989b37b78c7a
Jeremy Johnstone <github@jeremyjohnstone.com>
2011-06-24 16:47:29 -0700

    General improvements in login/logout handling related to other browser tabs + JS insertion cleanup 1) Moved Javascript to an external file and enabled async loading of LinkedIn framework 2) Fixed an issue where if you were logged in already to LI and previously authed, that it would then auto sign you into drupal 3) Fixed an issue where if you signed out in another tab / window that it would then sign you out in drupal too 4) Added an entropy cachebust to work around a cache bug in Safari / Chrome / Opera in regards to userspace.js

---
+++ linkedin_sso.js
@@ -0,0 +1,70 @@
+jQuery(document).ready(function document_ready_cb() {
+	jQuery.getScript("https://platform.linkedin.com/in.js?async=true", function framework_loaded_cb() {
+		var drupal_linkedin_sso_return_uri = encodeURIComponent(window.location.href.replace(/^http:\/\//, 'https://'));
+		var drupal_linkedin_sso_login_uri = Drupal.settings.linkedin_sso.base_url.replace(/^http:\/\//, 'https://') + '/linkedin_sso/login?return=' + drupal_linkedin_sso_return_uri;
+		var drupal_linkedin_sso_loggedout_home_uri = Drupal.settings.linkedin_sso.base_url.replace(/^https:\/\//, 'http://');
+		var drupal_linkedin_sso_logout_uri = Drupal.settings.linkedin_sso.base_url.replace(/^http:\/\//, 'https://') + '/user/logout?no_jsapi_logout=1';
+		
+		IN.Event.on(IN, 'frameworkLoaded', function() {
+			IN.Event.on(IN, 'logout', function() {
+				setTimeout(function() {
+					window.location.href = drupal_linkedin_sso_loggedout_home_uri;
+				}, 2000);
+			});
+
+			if(!IN.User.isAuthorized()) {
+				// user isn't LI authed, so register handler for if that happens to redirect to Drupal side login handler
+				IN.Event.onOnce(IN, 'auth', function() {
+					window.location.href = drupal_linkedin_sso_login_uri;
+				});
+				if(Drupal.settings.linkedin_sso.active_session) {
+					window.location.href = drupal_linkedin_sso_logout_uri;
+				}
+			} else {
+				// if user is LI authed, but doesn't have a drupal active session, redirect them to do Drupal login
+				if(!Drupal.settings.linkedin_sso.active_session) {
+					window.location.href = drupal_linkedin_sso_login_uri + '&existing_auth=1';
+				}
+			}
+			
+			try {
+				if(drupal_linkedin_sso_load_callback && drupal_linkedin_sso_load_callback.call) {
+					drupal_linkedin_sso_load_callback();
+				}
+			} catch(e) {
+				
+			}
+		});
+		
+		function user_login_link_click_handler(event) {
+			try { 
+				if(IN.User.authorize()) {
+					event.preventDefault();
+				}
+			}
+			catch(e) { 
+				return; 
+			}
+		}
+		
+		// attach LinkedIn SSO login handler
+		jQuery('a[href="/user"]').click(user_login_link_click_handler);
+		jQuery('a[href="/user/login"]').click(user_login_link_click_handler);
+
+		// registration links point to linkedin.com
+		jQuery('a[href="/user/register"]').attr({
+			href: "http://www.linkedin.com",
+			target: "_blank"
+		});
+		
+		IN.init({
+		    api_key: Drupal.settings.linkedin_sso.api_key,
+			credentials_cookie: "true",
+			authorize: "true",
+			cache_buster: Drupal.settings.linkedin_sso.entropy
+		});
+	});
+});
+
+
+
